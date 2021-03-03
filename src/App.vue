d42a6d5b4f16b285b04dcfdbe4b8bb7c025bf354
“Zhou” <1684133967@qq.com>
2021-03-03 19:54:51 +0800

    徐静3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
       <p>送你三月的风 六月的雨 九月的风景</p>
+      <p>       <p>
       <p>每个梦境 每次清醒 陪伴的长情</p>
-   <img src="https://sub-account-zhou.oss-cn-shanghai.aliyuncs.com/b-ssl.duitang.jpg" ,height = 100,width = 100/>
+      <p>       <p>
+   <img src="https://sub-account-zhou.oss-cn-shanghai.aliyuncs.com/b-ssl.duitang.jpg"/>
       <p>徐静，送你的花哈哈哈<p>
-      <p>让我余生都有关于你</p>
   </div>
 </template>
 
